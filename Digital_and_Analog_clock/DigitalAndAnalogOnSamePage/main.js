91ae4a4731327e3a3bf0e1474367ad57c92c89eb
Anisur Islam <aianis012355@gmail.com>
2025-10-20 21:03:01 +0600

    added Digital and Analog Clock

---
+++ Digital_and_Analog_clock/DigitalAndAnalogOnSamePage/main.js
@@ -0,0 +1,61 @@
+var container = document.getElementById("containers")
+var fullScreenButton = document.getElementById("full_screen_button")
+
+var dates = document.getElementById("dates")
+var time = document.getElementById("time")
+
+var hoursDiv = document.getElementById("hours")
+var minutesDiv = document.getElementById("minutes")
+var secondsDiv = document.getElementById("seconds")
+var amOrPmDiv = document.getElementById("am_or_pm")
+
+
+
+
+setInterval(() => {
+    let date1 = new Date();
+    let date2 = new Date();
+    let stylesForDate = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    }
+
+    let clockForDate = date1.toLocaleDateString("en-us", stylesForDate)
+    dates.innerHTML = clockForDate;
+
+
+
+    let dateHours = date2.getHours();
+    let dateMinutes = date2.getMinutes();
+    let dateSeconds = date2.getSeconds();
+
+    if (dateHours > 12) {
+        dateHours = `${dateHours - 12}`
+        amOrPmDiv.innerText = "PM"
+    } else if (dateHours == 12) {
+        amOrPmDiv.innerText = "PM"
+    } else if (dateHours == 0) {
+        dateHours = 12
+        amOrPmDiv.innerText = "AM"
+    } else {
+        dateHours = dateHours;
+        dateMinutes = date2.getMinutes();
+        dateSeconds = date2.getSeconds();
+        amOrPmDiv.innerText = "AM"
+    };
+    if (dateHours < 10) {
+        dateHours = `0${dateHours}`
+    }
+    if (dateMinutes < 10) {
+        dateMinutes = `0${dateMinutes}`
+    }
+    if (dateSeconds < 10) {
+        dateSeconds = `0${dateSeconds}`
+    }
+    hoursDiv.textContent = dateHours;
+    minutesDiv.textContent = dateMinutes;
+    secondsDiv.textContent = dateSeconds;
+
+}, 1000)
---
+++ Digital_and_Analog_clock/analog.js
@@ -0,0 +1,35 @@
+var hourPointer = document.getElementById("hour_pointer")
+var minutePointer = document.getElementById("minute_pointer")
+var secondPointer = document.getElementById("second_pointer")
+var container = document.getElementById("containers")
+var fullScreenButton = document.getElementById("full_screen_button")
+
+let date3 = new Date();
+hourPointer.style.transform = `rotate(${((30 * date3.getHours()) + (date3.getMinutes() / 2)) + (date3.getMinutes() / 30)}deg)`
+minutePointer.style.transform = `rotate(${6 * date3.getMinutes() + (date3.getSeconds() / 10)}deg)`
+secondPointer.style.transform = `rotate(${6 * date3.getSeconds()}deg)`
+
+var hourPointerRotation = 30 / 3600
+var minutePointerRotation = 6 / 60
+var secondPointerRotation = 6
+
+fullScreenButton.addEventListener("click", () => {
+    if (fullScreenButton.innerText == "Enter Fullscreen") {
+        container.requestFullscreen();
+        fullScreenButton.innerText = "Exit Fullscreen"
+    } else {
+        document.exitFullscreen()
+        fullScreenButton.innerText = "Enter Fullscreen"
+    }
+    }
+)
+setInterval(() => {
+
+
+
+    hourPointer.style.transform += `rotate(${hourPointerRotation}deg)`
+    minutePointer.style.transform += `rotate(${minutePointerRotation}deg)`
+    secondPointer.style.transform += `rotate(${secondPointerRotation}deg)`
+
+}, 1000)
+
